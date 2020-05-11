454e69225ac64b4d5002f2118e16c2c30a6873dc
cogullu <cogullu@users.noreply.github.com>
2020-05-11 17:08:57 +0300

    Update kucukHarf.py

--- kucukHarf.py
+++ kucukHarf.py
@@ -1,4 +1,4 @@
-#Tümünü Küçük harfe çeviren program.
+#Tümünü Küçük veya büyük harflere çeviren program.
 
 buyukAlfabe = "ABCÇDEFGĞHIİJKLMNOÖPRSŞTUÜVYZ"
 kucukAlfabe = "abcçdefgğhıijklmnoöprsştuüvyz"
@@ -9,6 +9,17 @@ def kucukHarfCevir(text:str):
         if i in buyukAlfabe:
             index = buyukAlfabe.index(i)
             newText += kucukAlfabe[index]
+        else:
+            newText += i
+
+    return newText
+
+def buyukHarfCevir(text:str):
+    newText = str()
+    for i in text:
+        if i in kucukAlfabe:
+            index = kucukAlfabe.index(i)
+            newText += buyukAlfabe[index]
         else:
             newText += i
 
